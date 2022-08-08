3302afa8c4cefdec09762038eda8947aceee6186
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-08-08 13:52:36 -0600

    Prueba2 Multiple Checkbox

--- mostrar.php
+++ mostrar.php
@@ -13,13 +13,16 @@
     echo "<h3>Genero: </h3>".$_POST['masculino-femenino'];
     //echo "<h3>Dia: </h3>".$_POST['dias-semana'];  
 
+
+
     if(isset($_POST['submit'])){
-        if(!empty($_POST['dias-semana'])){
-        // Loop to store and display values of individual checked checkbox.
-        foreach($_POST['dias-semana'] as $selected){
-        echo $selected."</br>";
+
+        $brands = $_POST['dias-semana'];
+        foreach($brands as $item){
+echo $item . "<br>"
+
         }
-        }
+
         }
     ?>
   
