f98dff976ac2e1810dbd41b6d9fc810a5b8861c7
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2022-08-08 13:25:24 -0600

    Initial commit

---
+++ mostrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    echo "<h3>Nombre: </h3>".$_POST['nombre'];
+    echo "<h3>Genero: </h3>".$_POST['masculino-femenino'];
+    echo "<h3>Dia: </h3>".$_POST['dias-semana'];  
+    ?>
+  
+
+  
+
+</body>
+</html>
